3406f5960a04041fafc870b48e78b971259e76ab
Yasset Perez-Riverol <ypriverol@gmail.com>
2026-03-11 07:29:08 +0000

    feat: add SomaScan validator and validator dispatch

---
+++ src/pyap/validators/__init__.py
@@ -0,0 +1,26 @@
+from pyap.core import AffinityDataset, Platform, ValidationResult
+from pyap.validators.olink_explore import OlinkExploreValidator
+from pyap.validators.olink_target import OlinkTargetValidator
+from pyap.validators.somascan import SomaScanValidator
+
+_VALIDATORS = {
+    Platform.OLINK_EXPLORE: OlinkExploreValidator,
+    Platform.OLINK_EXPLORE_HT: OlinkExploreValidator,
+    Platform.OLINK_TARGET: OlinkTargetValidator,
+    Platform.SOMASCAN: SomaScanValidator,
+}
+
+
+def validate(dataset: AffinityDataset) -> list[ValidationResult]:
+    validator_cls = _VALIDATORS.get(dataset.platform)
+    if validator_cls is None:
+        raise ValueError(f"No validator for platform: {dataset.platform}")
+    return validator_cls().validate(dataset)
+
+
+__all__ = [
+    "validate",
+    "OlinkExploreValidator",
+    "OlinkTargetValidator",
+    "SomaScanValidator",
+]
